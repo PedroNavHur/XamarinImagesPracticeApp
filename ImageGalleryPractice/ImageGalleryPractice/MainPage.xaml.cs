ec2e5bd721b82f7d99a2d83f733ee74bf8e2b3e7
Pedro Navarrete Hurtado <navhpedro@gmail.com>
2017-12-27 20:47:12 -0600

    Left Button

--- ImageGalleryPractice/ImageGalleryPractice/MainPage.xaml.cs
+++ ImageGalleryPractice/ImageGalleryPractice/MainPage.xaml.cs
@@ -7,11 +7,39 @@ using Xamarin.Forms;
 
 namespace ImageGalleryPractice
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class MainPage : ContentPage
+    {
+        private Int16 _pictureId = 1;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+
+        }
+
+        private void LoadImage()
+        {
+            image.Source = new UriImageSource
+            {
+                Uri = new Uri(String.Format("http://lorempixel.com/1920/1080/city/{0}", _pictureId)),
+                CachingEnabled = false,
+                CacheValidity = TimeSpan.FromHours(1)
+            };
+        }
+
+
+        private void LeftButton(Object sender, EventArgs e)
+        {
+            _pictureId--;
+            if (_pictureId == 0)
+                _pictureId = 10;
+
+            LoadImage();
+        }
+
+
+
+    }
 }
